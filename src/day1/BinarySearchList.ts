38975d56a01670fda736e6bea7eb68196cb94a8e
Jason Clark <clarkjasonee@gmail.com>
2023-02-15 17:12:41 -0500

    added comments to each algo completed so far

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,29 @@
-export default function bs_list(haystack: number[], needle: number): boolean {
+// Use binary search to find the value needle in the array haystack.
+// The binary search technique consists of halving the array and checking the value of that index to be <=> to the value we're looking for.
+// This process continues until the value is found or the array can no longer be reduced in size.
+// This results in O(logN) time complexity - log is just the inverse of exponentiation so log(base2) of a number will equal the power of 2 to get that number.
+// For example, if the array is 4096 elements long, it will require a maximum of 12 iterations of the BST algorithm to find the answer: 2^12 = 4096.
 
+export default function bs_list(haystack: number[], needle: number): boolean {
+  let lo = 0;
+  let hi = haystack.length;
+
+  do {
+    // calculate the midpoint of the current subsection of the array
+    const m = Math.floor(lo + ((hi - lo) / 2));
+    const v = haystack[m];
+    if (v === needle) {
+      // if the value at the midpoint equals the needle, return true
+      return true;
+    } else if (v < needle) {
+      // if the value at the midpoint is less than needle, reassign lo to the midpoint + one
+      lo = m + 1;
+    } else {
+      // if the value at the midpoint is greater than needle, reassign hi to the midpoint
+      hi = m;
+    }
+    // continue halving the array while lo is less than hi
+  } while (lo < hi);
+  // if we make it through the entire array without finding the needle, return false
+  return false;
 }
--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,52 @@
-export default function two_crystal_balls(breaks: boolean[]): number {
+// Given two crystal balls that will break if dropped from a high enough distance, determine the exact spot in which it will break in the most optimized way.
+// This solution results in O(sqrt(N)) time complexity
+// I still question whether this is really any different than O(N) since the sqrt piece is effectively a constant...but it is a different constant depending on the size of the array.
 
+export default function two_crystal_balls(breaks: boolean[]): number {
+  // define our jump interval as square root of array length
+  const jump = Math.floor(Math.sqrt(breaks.length));
+
+  let i = jump;
+  for (; i < breaks.length; i += jump) {
+    if (breaks[i]) {
+      // if a crystal ball breaks at this index, go back to the last known safe index and traverse from there back to this index
+      break;
+    }
+  }
+
+  // jump back one interval (plus one since we know one interval back did not break) and traverse this portion of the array with jumps of one
+  i = i - jump + 1;
+
+  for (; i < breaks.length; i++) {
+    if (breaks[i]) {
+      // if the second ball breaks at this index, return the index
+      return i;
+    }
+  }
+  // if we made it this far with no break, return -1
+  return -1;
 }
+
+
+// NOTE: example version
+// I think my second for loop is cleaner without using j, but interesting that you can have two+ variables increment in a for loop
+// Also check what's the significance of ++i vs. i++ in this particular case
+// export default function two_crystal_balls(breaks: boolean[]): number {
+//   const jump = Math.floor(Math.sqrt(breaks.length));
+
+//   let i = jump;
+//   for (; i < breaks.length; i += jump) {
+//     if (breaks[i]) {
+//       break;
+//     }
+//   }
+
+//   i -= jump;
+
+//   for (let j = 0; j < jump && i < breaks.length; ++j, ++i) {
+//     if (breaks[i]) {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
