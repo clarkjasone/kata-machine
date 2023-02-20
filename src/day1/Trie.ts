e1f1f424b95ed1274408b657122ab597cf0d7969
Jason Clark <clarkjasonee@gmail.com>
2023-02-19 19:17:01 -0500

    added helper function to trie

--- src/day1/Trie.ts
+++ src/day1/Trie.ts
@@ -1,18 +1,24 @@
 export default class Trie {
     
-
-    
-
     constructor() {
     }
 
     insert(item: string): void {
 
-}
+    }
     delete(item: string): void {
 
-}
+    }
     find(partial: string): string[] {
 
+    }
 }
-}
+
+
+// example character code index function
+const zero = "a".charCodeAt(0);
+console.log(zero);
+
+function idx(str) { return str.charCodeAt(0) - zero}
+
+console.log(idx("c"));
