d19b2bebd23ab0510ec5e6f2bfb8134f952e649e
Jason Clark <clarkjasonee@gmail.com>
2023-02-21 19:04:00 -0500

    implemented DFSGraphList

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,4 +1,7 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+export default function bfs(
+  graph: WeightedAdjacencyMatrix,
+  source: number, 
+  needle: number): number[] | null {
 
   const seen = new Array(graph.length).fill(false);
   const prev = new Array(graph.length).fill(-1);
